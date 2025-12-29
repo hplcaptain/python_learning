3a207c7bba073e57c2cec4cd2aa719d58a003419
hplcaptain <hploffice365@gmail.com>
2025-12-29 18:10:36 +0530

    Update 1

---
+++ Assignment 1/Task 1.py
@@ -0,0 +1,13 @@
+numbers1 = float(input("Enter the first digit: "))
+numbers2 = float(input("Enter the second digit: "))
+
+addition = numbers1 + numbers2
+subtraction = numbers1 - numbers2
+multiplication = numbers1 * numbers2
+division = numbers1 / numbers2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+

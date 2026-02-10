fb2fa49839f6e431264fa909984466eac6220d5f
OSP - Repo <teamrepo@osplabs.com>
2026-02-10 14:03:26 +0530

    Task 2 Added

---
+++ Assignment 5/task_2.py
@@ -0,0 +1,8 @@
+numbers = list(range(1,11))
+
+first_five = numbers[:5]
+reverse_list = list(reversed(first_five))
+
+print("Original list: ", numbers)
+print("Extracted first five elements: ",first_five)
+print("Reverse extracted elements: ",reverse_list)
